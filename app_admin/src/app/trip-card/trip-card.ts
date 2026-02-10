8d356b060f553527e0c803658319b88a5c05fa99
NebuFlux <joshua.shoemaker@protonmail.me>
2026-02-09 21:10:00 -0500

    Angular implementation started. error connecting to api/trips

---
+++ app_admin/src/app/trip-card/trip-card.ts
@@ -0,0 +1,20 @@
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-card',
+  standalone: true,
+  imports: [CommonModule, CurrencyPipe],
+  templateUrl: './trip-card.html',
+  styleUrl: './trip-card.css',
+})
+export class TripCard implements OnInit {
+
+  @Input('trip') trip: any;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
+}
---
+++ app_admin/src/app/trip-listing/trip-listing.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Trip } from '../models/trip';
+import { TripData } from '../services/trip-data'
+import { TripCard } from '../trip-card/trip-card';
+
+@Component({
+  selector: 'app-trip-listing',
+  imports: [CommonModule, TripCard],
+  templateUrl: './trip-listing.html',
+  styleUrl: './trip-listing.css',
+  providers: [TripData]
+})
+export class TripListing implements OnInit {
+  trips!: Trip[];
+  message: string ='';
+
+  constructor(private tripData: TripData) {
+    console.log('trip-listing constructor');
+  }
+
+  private getStuff(): void {
+    this.tripData.getTrips()
+      .subscribe({
+        next: (value: any) => {
+          this.trips = value;
+          if(value.length > 0)
+          {
+            this.message = 'there are ' + value.length + ' trips available.';
+          }
+          else {
+            this.message = 'There were no trips retrieved from the database';
+          }
+          console.log(this.message);
+        },
+        error: (error: any) => {
+          console.log('Error: ' + error);
+        }
+      })
+  }
+
+  ngOnInit(): void{
+    console.log('ngOnInit');
+    this.getStuff();
+  }
+}
